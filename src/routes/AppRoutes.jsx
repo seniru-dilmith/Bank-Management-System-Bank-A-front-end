6237e0cf203af78d6168e8739acecb1c7b67e35c
KasunyaH <kasunyahimani@gmail.com>
2024-09-27 08:52:00 +0530

    AppRoutes changed

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -6,6 +6,9 @@ import Login from '../pages/Login';
 import Settings from '../pages/Settings';
 import CustomerSupport from '../pages/CustomerSupport';
 import CustomerDashboard from '../pages/CustomerDashboard';
+import Transactions from '../pages/Transactions';
+import Loans from '../pages/Loans';
+import CHelp from '../pages/CHelp';
 import ManagerDashboard from '../pages/ManagerDashboard';
 import EmployeeDashboard from '../pages/EmployeeDashboard';
 
@@ -18,6 +21,9 @@ const AppRoutes = () => {
       <Route path="/settings" element={<Settings />} />
       <Route path="/customersupport" element={<CustomerSupport />} />
       <Route path="/customer" element={<CustomerDashboard />} />
+      <Route path="/customer/transactions" element={<Transactions />} />
+      <Route path="/customer/loans" element={<Loans />} />
+      <Route path="/customer/chelp" element={<CHelp />} />
       <Route path="/manager" element={<ManagerDashboard />} />
       <Route path="/employee" element={<EmployeeDashboard />} />
     </Routes>
