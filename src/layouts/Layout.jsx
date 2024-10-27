05b62cc2140b3e99ae7c6a8b0e2d7efe30610fd3
Sasmitha Uvindu Jayasinghe <158791024+jayasinghesasmitha@users.noreply.github.com>
2024-10-27 12:59:08 +0530

    Update Layout.jsx

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -10,7 +10,7 @@ const Layout = ({ children, NavigationBar }) => {
       <div className="header">
         <div className="logo-container">
           <img src={logo} alt="Bank Logo" className="logo" />
-          <h2 className="bank-name">ABC Online Banking</h2> 
+          <h2 className="bank-name">A Online Banking</h2> 
         </div>
         <div className="nav-container">
           {NavigationBar}
@@ -26,7 +26,7 @@ const Layout = ({ children, NavigationBar }) => {
         {/* Logo and description */}
         <div className="footer-logo-section">
           <img src={logo} alt="Bank Logo" className="logo" />
-          <h2 className="bank-name">ABC Online Banking</h2> 
+          <h2 className="bank-name">A Online Banking</h2> 
           <p>
             Safe Trust and Future
           </p>
