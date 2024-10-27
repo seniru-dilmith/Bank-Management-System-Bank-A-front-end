f215e65fd8656201332613cb47b00310da6bfa3d
seniru-dilmith <dilmithseniru@gmail.com>
2024-10-27 17:01:10 +0530

    Revert "Final css changes"

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import './layout.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebookF, faTwitter, faInstagram, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
 import logo from './ABCLogo.png'; // Adjust the path to your logo
 
 const Layout = ({ children, NavigationBar }) => {
@@ -10,7 +8,7 @@ const Layout = ({ children, NavigationBar }) => {
       <div className="header">
         <div className="logo-container">
           <img src={logo} alt="Bank Logo" className="logo" />
-          <h2 className="bank-name">A Online Banking</h2> 
+          <h2 className="bank-name">ABC Online Banking</h2> 
         </div>
         <div className="nav-container">
           {NavigationBar}
@@ -21,63 +19,7 @@ const Layout = ({ children, NavigationBar }) => {
         <div className="gradient-box-right"></div>
       </div>
       <div className="content-container">{children}</div> {/* Ensure children are rendered */}
-      <footer className="footer">
-      <div className="footer-container">
-        {/* Logo and description */}
-        <div className="footer-logo-section">
-          <img src={logo} alt="Bank Logo" className="logo" />
-          <h2 className="bank-name">ABC Online Banking</h2> 
-          <p>
-            Safe Trust and Future
-          </p>
-          <div className="social-icons">
-          <span className="icon-circle">
-          <FontAwesomeIcon icon={faFacebookF} />
-        </span>
-        <span className="icon-circle">
-          <FontAwesomeIcon icon={faTwitter} />
-        </span>
-        <span className="icon-circle">
-          <FontAwesomeIcon icon={faInstagram} />
-        </span>
-        <span className="icon-circle">
-          <FontAwesomeIcon icon={faLinkedinIn} />
-        </span>
-          </div>
-        </div>
-
-        {/* Links sections */}
-        <div className="footer-links">
-          <h4>Product</h4>
-          <ul>
-            <li>Accounts</li>
-            <li>Loans</li>
-            <li>Deposits</li>
-            <li>Transactions</li>
-            <li>Reports</li>
-          </ul>
-        </div>
-
-        <div className="footer-links">
-          <h4>Contact Us</h4>
-          <ul>
-            <li><b>Hotline</b></li>
-            <li>0112 345 678</li>
-            <li><b>Mobile Number</b></li>
-            <li>071 123 4568</li>
-            <li><b>Email</b></li>
-            <li>abcbank@gmail.com</li>
-          </ul>
-        </div>
-
-        <div className="footer-address">
-          <h4>Address</h4>
-          <p>No.123,University of Moratuwa,Katubadda</p>
-        </div>
-      </div>
-    </footer>
     </div>
-    
   );
 };
 
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -23,12 +23,10 @@ const HomePage = () => {
 
   const paragraphStyle = {
     fontSize: '1.2em',
-    textAlign: 'left',
     lineHeight: '1.5',
     maxWidth: '800px',
     color: 'rgba(255, 255, 255, 0.85)', // Adjusted for contrast
   };
-  
 
   return (
     <Layout NavigationBar={<HomeNaviBar />}>
